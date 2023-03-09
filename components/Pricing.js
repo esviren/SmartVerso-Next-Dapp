3d667afecf5b2c49f0e25d06f44e9536242c71fc
esviren <st-rodriguez@hotmail.com>
2023-03-09 14:39:48 -0500

    add project SmartLAB

---
+++ components/Pricing.js
@@ -0,0 +1,275 @@
+import React, { useMemo } from "react";
+import Image from "next/image";
+import Testimoni from "./Testimoni";
+import ButtonPrimary from "./misc/ButtonPrimary";
+import ButtonOutline from "./misc/ButtonOutline.";
+import Maps from "../public/assets/HugeGlobal.svg";
+import { motion } from "framer-motion";
+import getScrollAnimation from "../utils/getScrollAnimation";
+import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
+
+const Pricing = () => {
+  const scrollAnimation = useMemo(() => getScrollAnimation(), []);
+
+  return (
+    <div
+      className="bg-gradient-to-b from-white-300 to-white-500 w-full py-14"
+      id="pricing"
+    >
+      <div className="max-w-screen-xl  px-6 sm:px-8 lg:px-16 mx-auto flex flex-col w-full text-center justify-center">
+        <div className="flex flex-col w-full">
+          <ScrollAnimationWrapper>
+            <motion.h3
+              variants={scrollAnimation}
+              className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-relaxed"
+            >
+              SmartVerso
+            </motion.h3>
+            <motion.p
+              variants={scrollAnimation}
+              className="leading-normal w-10/12 sm:w-7/12 lg:w-6/12 mx-auto my-2 text-center"
+            >
+              Let's choose the Smartverso area that is best for you and explore it happily
+              and cheerfully.
+            </motion.p>
+          </ScrollAnimationWrapper>
+          <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-3 gap-4 lg:gap-12 py-8 lg:py-12 px-6 sm:px-0 lg:px-6">
+            <ScrollAnimationWrapper className="flex justify-center">
+              <motion.div
+                variants={scrollAnimation}
+                className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl py-4 px-6 lg:px-12 xl:px-20"
+                whileHover={{
+                  scale : 1.1,
+                  transition: {
+                    duration: .2
+                  }
+                }}
+              >
+                <div className="p-4 lg:p-0 mt-6 lg:mt-16">
+                  <Image
+                    src="/assets/Free.png"
+                    width={145}
+                    height={165}
+                    alt="Free Plan"
+                  />
+                </div>
+                <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
+                  Free Play
+                </p>
+                <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
+                  <li className="relative check custom-list my-2">
+                    sube tus modelos 3D
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Vende NFTs
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Mini Juegos
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Interactua comunidad 3D
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Gana SmartVesro Coins
+                  </li>
+                </ul>
+                <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12">
+                  <p className="text-2xl text-black-600 text-center mb-4 ">
+                    Free Play
+                  </p>
+                  <ButtonOutline>Conoce Más</ButtonOutline>
+                </div>
+              </motion.div>
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper className="flex justify-center">
+              <motion.div
+                variants={scrollAnimation}
+                className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl py-4 px-6 lg:px-12 xl:px-20"
+                whileHover={{
+                  scale : 1.1,
+                  transition: {
+                    duration: .2
+                  }
+                }}
+              >
+              <div className="p-4 lg:p-0 mt-6 lg:mt-16">
+                <Image
+                  src="/assets/Standard.png"
+                  width={145}
+                  height={165}
+                  alt="Standard Plan"
+                />
+              </div>
+              <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
+                Tierras {"NFTs"}
+              </p>
+              <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
+                <li className="relative check custom-list my-2">
+                  Inicia en zona caliente
+                </li>
+                <li className="relative check custom-list my-2">
+                  realiza eventos 
+                </li>
+                <li className="relative check custom-list my-2">
+                  Personaliza tu espacio
+                </li>
+                <li className="relative check custom-list my-2">
+                  Comercio de Tierras NFT
+                </li>
+                <li className="relative check custom-list my-2">
+                  {"Propiedad Raiz Digital "}
+                </li>
+              </ul>
+              <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12">
+                <p className="text-2xl text-black-600 text-center mb-4 ">
+                  Pioneros <span className="text-black-500">/ mo</span>
+                </p>
+                <ButtonOutline>Conoce Más</ButtonOutline>
+              </div>
+              </motion.div>
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper className="flex justify-center">
+              <motion.div
+                variants={scrollAnimation}
+                className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl py-4 px-6 lg:px-12 xl:px-20"
+                whileHover={{
+                  scale : 1.1,
+                  transition: {
+                    duration: .2
+                  }
+                }}
+              >
+              <div className="p-4 lg:p-0 mt-6 lg:mt-16">
+                <Image
+                  src="/assets/Premium.png"
+                  width={145}
+                  height={165}
+                  alt="Premium Plan"
+                />
+              </div>
+              <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
+                Espacios Comerciales{" "}
+              </p>
+              <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
+                <li className="relative check custom-list my-2">
+                  Comercio Experiencial
+                </li>
+                <li className="relative check custom-list my-2">
+                  Vende Productos en el Metaverso
+                </li>
+                <li className="relative check custom-list my-2">
+                  Posisionamiento de marca
+                </li>
+                <li className="relative check custom-list my-2">
+                  Productos NFT
+                </li>
+                <li className="relative check custom-list my-2">
+                  Eventos en {"El Metaverso "}
+                </li>
+                <li className="relative check custom-list my-2">
+                  Clientes {"en liena"}
+                </li>
+              </ul>
+              <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12">
+                <p className="text-2xl text-black-600 text-center mb-4 ">
+                  Empresarial <span className="text-black-500">VR</span>
+                </p>
+
+                <ButtonOutline>Conoce Más</ButtonOutline>
+              </div>
+              </motion.div>
+            </ScrollAnimationWrapper>
+          </div>
+        </div>
+        <div className="flex flex-col w-full my-16">
+          <ScrollAnimationWrapper>
+            <motion.h3
+              variants={scrollAnimation}
+              className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-relaxed w-9/12 sm:w-6/12 lg:w-4/12 mx-auto">
+              SmartVerso Educación sin límites{" "}
+            </motion.h3>
+            <motion.p className="leading-normal  mx-auto my-2 w-10/12 sm:w-7/12 lg:w-6/12" variants={scrollAnimation}>
+              In nec mattis eros. Morbi porta dolor nunc, eu congue velit semper a. Quisque et turpis mauris.
+               Phasellus accumsan arcu sit amet aliquam lobortis. Aliquam volutpat vestibulum justo eu ullamcorper. 
+            </motion.p>
+          </ScrollAnimationWrapper>
+          <ScrollAnimationWrapper>
+            <motion.div className="py-12 w-full px-8 mt-16" variants={scrollAnimation}> 
+              <Maps className="w-full h-auto" />
+            </motion.div>
+          </ScrollAnimationWrapper>
+          <ScrollAnimationWrapper>
+            <motion.div className="w-full flex justify-evenly items-center mt-4 flex-wrap lg:flex-nowrap" variants={scrollAnimation}>
+              {/* <Netflix className="h-18 w-auto" /> */}
+              <img
+                src="/assets/Icon/amazon.png"
+                className="h-14 w-auto mt-4 lg:mt-2"
+                alt=""
+              />
+              <img
+                src="/assets/Icon/netflix.png"
+                className="h-14 w-auto mt-2 lg:mt-0"
+                alt=""
+              />
+              <img
+                src="/assets/Icon/reddit.png"
+                className="h-12 w-auto mt-2 lg:mt-0"
+                alt=""
+              />
+              <img
+                src="/assets/Icon/discord.png"
+                className="h-14 w-auto mt-2 lg:mt-0"
+                alt=""
+              />
+              <img
+                src="/assets/Icon/spotify.png"
+                className="h-12 w-auto mt-2 lg:mt-0"
+                alt=""
+              />
+            </motion.div>
+          </ScrollAnimationWrapper>
+        </div>
+        <div className="flex flex-col w-full my-16" id="testimoni">
+          <ScrollAnimationWrapper>
+            <motion.h3
+              variants={scrollAnimation}
+              className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-normal w-9/12 sm: lg:w-4/12 mx-auto">
+              Nuestro Equipo {" "}
+            </motion.h3>
+            <motion.p
+              variants={scrollAnimation}
+              className="leading-normal mx-auto mb-2 mt-4 w-10/12 sm:w-7/12 lg:w-6/12"
+            >
+               Aenean id massa eget nisi suscipit mollis. Maecenas eget sem a tortor malesuada tincidunt quis nec eros.
+                Ut quis faucibus lectus, id consectetur lectus
+            </motion.p>
+          </ScrollAnimationWrapper>
+          <ScrollAnimationWrapper className="w-full flex flex-col py-12">
+            <motion.div variants={scrollAnimation}>
+              <Testimoni />
+            </motion.div>
+          </ScrollAnimationWrapper>
+          <ScrollAnimationWrapper className="relative w-full mt-16">
+            <motion.div variants={scrollAnimation} custom={{duration: 3}}>
+              <div className="absolute rounded-xl  py-8 sm:py-14 px-6 sm:px-12 lg:px-16 w-full flex flex-col sm:flex-row justify-between items-center z-10 bg-white-500">
+                <div className="flex flex-col text-left w-10/12 sm:w-7/12 lg:w-5/12 mb-6 sm:mb-0">
+                  <h5 className="text-black-600 text-xl sm:text-2xl lg:text-3xl leading-relaxed font-medium">
+                    Suscribete <br /> Para conocer nustros desarrollo!
+                  </h5>
+                  <p>Permanese al dia en SmartLab</p>
+                </div>
+                <ButtonPrimary>Suscríbete</ButtonPrimary>
+              </div>
+              <div
+                className="absolute bg-black-600 opacity-5 w-11/12 roudned-lg h-60 sm:h-56 top-0 mt-8 mx-auto left-0 right-0"
+                style={{ filter: "blur(114px)" }}
+                ></div>
+            </motion.div>
+          </ScrollAnimationWrapper>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
