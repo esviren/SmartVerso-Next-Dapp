0af87336978433816f646fe58046f14f88f33f5e
esviren <st-rodriguez@hotmail.com>
2023-03-14 18:48:24 -0500

    test commit linux lenovo office

--- components/Feature.js
+++ components/Feature.js
@@ -26,7 +26,7 @@ const Feature = () => {
           <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
             <Image
               src="/assets/Illustration2.png"
-              alt="VPN Illustrasi"
+              alt="SmartLAB Illustrasi"
               layout="responsive"
               quality={100}
               height={414}
