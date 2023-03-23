35d2007d7985463f21536f3ced9b6ae035b1abde
esviren <st-rodriguez@hotmail.com>
2023-03-22 19:41:19 -0500

    add new text from portafoli PDF

--- components/Feature.js
+++ components/Feature.js
@@ -19,10 +19,10 @@ const Feature = () => {
 
   return (
     <div
-      className="max-w-screen-xl mt-8 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto"
+      className="max-w-screen-xl mt-32 mb-6 sm:mt-18 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto"
       id="feature"
     >
-      <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8 p  y-8 my-12">
+      <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-4 p  y-8 my-8">
         <ScrollAnimationWrapper className="flex w-full justify-end">
           <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
             <Image
@@ -40,7 +40,7 @@ const Feature = () => {
           <h3 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
             Experiencias Tecnologicas Para Diferentes Dispositivos
           </h3>
-          <p className="my-2 text-black-500">
+          <p className="my-2 text-black-500 text-justify">
             SmartLab S.A.S es un equipo apasionado de expertos en realidad virtual, con una
             amplia experiencia en diseño, programación y creación de contenido en este campo.
             Nuestro objetivo es crear experiencias de realidad virtual increíblemente inmersivas
