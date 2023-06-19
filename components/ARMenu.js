fd5613085e6c31ebdff6564954df132ffd582b23
esviren <st-rodriguez@hotmail.com>
2023-06-19 09:36:01 -0500

    change hero-restaurant & add new button to AR viewer

--- components/ARMenu.js
+++ components/ARMenu.js
@@ -56,12 +56,13 @@ const ARMenu = () => {
 
   return (
     <div
-      className="max-w-screen-xl mt-32 mb-6 sm:mt-18 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto"
+      className="max-w-screen-xl mt-32 mb-6 sm:mt-8 sm:mb-8 px-6 sm:px-8 lg:px-16 mx-auto"
       id="feature"
     >
       <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-4 p  y-8 my-8">
         <ScrollAnimationWrapper className="flex w-full justify-end">
           <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
+          <h2 className="text-4xl font-bold my-2 text-black-500">Menú AR</h2>
             <ARModelViewer
               src="/assets/3D/Calentado.glb"
               ios_src="/assets/3D/Calentado.usdz"
@@ -74,6 +75,7 @@ const ARMenu = () => {
           <h2 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
            Desayuno Calendo 
           </h2>
+          <h3 className="text-2xl lg:text-2xl font-medium leading-relaxed text-black-600">$18.000 COP</h3>
           <ul className="text-black-500 self-start list-inside ml-8">
             {ARCalentados.map((ARCalentado, index) => (
               <motion.li
@@ -92,7 +94,7 @@ const ARMenu = () => {
               )
             )}
           </ul>
-          <h3 className="text-2xl lg:text-2xl font-medium leading-relaxed text-black-600">$18.000 COP</h3>
+
         </motion.div>
         </ScrollAnimationWrapper>
       </div>
@@ -111,6 +113,7 @@ const ARMenu = () => {
           <h2 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
            Pizza Jamon Serrano
           </h2>
+          <h3 className="text-2xl lg:text-2xl font-medium leading-relaxed text-black-600">$42.000 COP</h3>
           <ul className="text-black-500 self-start list-inside ml-8">
             {ARPizzas.map((ARPizza, index) => (
               <motion.li
@@ -129,11 +132,87 @@ const ARMenu = () => {
               )
             )}
           </ul>
-          <h3 className="text-2xl lg:text-2xl font-medium leading-relaxed text-black-600">$42.000 COP</h3>
+
+        </motion.div>
+        </ScrollAnimationWrapper>
+      </div>
+      
+      <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-4 p  y-8 my-8">
+        <ScrollAnimationWrapper className="flex w-full justify-end">
+          <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
+            <ARModelViewer
+              src="/assets/3D/hamburger_2.glb"
+              ios_src="/assets/3D/hamburger_2.usdz"
+              alt="Hamburguesa Comercio Experiencial AR"
+            />
+          </motion.div>
+        </ScrollAnimationWrapper>
+        <ScrollAnimationWrapper>
+        <motion.div className="flex flex-col items-end justify-center ml-auto w-full lg:w-9/12" variants={scrollAnimation}>
+          <h2 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
+           2 Hamburguesa 125 Gr
+          </h2>
+          <h3 className="text-2xl lg:text-2xl font-medium leading-relaxed text-black-600">$35.000 COP</h3>
+          <ul className="text-black-500 self-start list-inside ml-8">
+            {ARfeatures.map((ARfeature, index) => (
+              <motion.li
+                className="relative circle-check custom-list"
+                custom={{duration: 2 + index}}
+                variants={scrollAnimation}
+                key={ARfeature}
+                whileHover={{
+                scale : 1.1,
+                transition: {
+                  duration: .2
+                }
+                }}>
+                  {ARfeature}
+              </motion.li>
+              )
+            )}
+          </ul>
         </motion.div>
         </ScrollAnimationWrapper>
       </div>
       <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-4 p  y-8 my-8">
+        <ScrollAnimationWrapper className="flex w-full justify-end">
+          <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
+
+            <ARModelViewer
+              src="/assets/3D/Aguagates.glb"
+              ios_src="/assets/3D/Aguagates.usdz"
+              alt="Amburguesa Comercio Experiencial AR"
+            />
+          </motion.div>
+        </ScrollAnimationWrapper>
+        <ScrollAnimationWrapper>
+        <motion.div className="flex flex-col items-end justify-center ml-auto w-full lg:w-9/12" variants={scrollAnimation}>
+          <h2 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
+           Ensalada De Frutas
+          </h2>
+          <ul className="text-black-500 self-start list-inside ml-8">
+            {ARfutas.map((ARfuta, index) => (
+              <motion.li
+                className="relative circle-check custom-list"
+                custom={{duration: 2 + index}}
+                variants={scrollAnimation}
+                key={ARfuta}
+                whileHover={{
+                scale : 1.1,
+                transition: {
+                  duration: .2
+                }
+                }}>
+                  {ARfuta}
+              </motion.li>
+              )
+            )}
+          </ul>
+          <h3 className="text-2xl lg:text-2xl font-medium leading-relaxed text-black-600">$10.000 COP</h3>
+        </motion.div>
+        </ScrollAnimationWrapper>
+      </div>
+      {/*<div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-4 p  y-8 my-8">
         <ScrollAnimationWrapper className="flex w-full justify-end">
           <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
             <ARModelViewer
@@ -169,81 +248,7 @@ const ARMenu = () => {
           <h3 className="text-2xl lg:text-2xl font-medium leading-relaxed text-black-600">$35.000 COP</h3>
         </motion.div>
         </ScrollAnimationWrapper>
-      </div>
-      <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-4 p  y-8 my-8">
-        <ScrollAnimationWrapper className="flex w-full justify-end">
-          <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
-            <ARModelViewer
-              src="/assets/3D/hamburger_2.glb"
-              ios_src="/assets/3D/hamburger_2.usdz"
-              alt="Hamburguesa Comercio Experiencial AR"
-            />
-          </motion.div>
-        </ScrollAnimationWrapper>
-        <ScrollAnimationWrapper>
-        <motion.div className="flex flex-col items-end justify-center ml-auto w-full lg:w-9/12" variants={scrollAnimation}>
-          <h2 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
-           2 Hamburguesa 125 Gr
-          </h2>
-          <ul className="text-black-500 self-start list-inside ml-8">
-            {ARfeatures.map((ARfeature, index) => (
-              <motion.li
-                className="relative circle-check custom-list"
-                custom={{duration: 2 + index}}
-                variants={scrollAnimation}
-                key={ARfeature}
-                whileHover={{
-                scale : 1.1,
-                transition: {
-                  duration: .2
-                }
-                }}>
-                  {ARfeature}
-              </motion.li>
-              )
-            )}
-          </ul>
-          <h3 className="text-2xl lg:text-2xl font-medium leading-relaxed text-black-600">$35.000 COP</h3>
-        </motion.div>
-        </ScrollAnimationWrapper>
-      </div>
-      <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-4 p  y-8 my-8">
-        <ScrollAnimationWrapper className="flex w-full justify-end">
-          <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
-            <ARModelViewer
-              src="/assets/3D/Aguagates.glb"
-              ios_src="/assets/3D/Aguagates.usdz"
-              alt="Amburguesa Comercio Experiencial AR"
-            />
-          </motion.div>
-        </ScrollAnimationWrapper>
-        <ScrollAnimationWrapper>
-        <motion.div className="flex flex-col items-end justify-center ml-auto w-full lg:w-9/12" variants={scrollAnimation}>
-          <h2 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
-           Ensalada De Frutas
-          </h2>
-          <ul className="text-black-500 self-start list-inside ml-8">
-            {ARfutas.map((ARfuta, index) => (
-              <motion.li
-                className="relative circle-check custom-list"
-                custom={{duration: 2 + index}}
-                variants={scrollAnimation}
-                key={ARfuta}
-                whileHover={{
-                scale : 1.1,
-                transition: {
-                  duration: .2
-                }
-                }}>
-                  {ARfuta}
-              </motion.li>
-              )
-            )}
-          </ul>
-          <h3 className="text-2xl lg:text-2xl font-medium leading-relaxed text-black-600">$10.000 COP</h3>
-        </motion.div>
-        </ScrollAnimationWrapper>
-      </div>
+      </div>*/}
     </div>
   );
 };
